0d0c2b994e49a7825ee718a65f7b25df51bc1cd3
Hannes Liik <hannes_liik@hotmail.com>
2019-02-16 11:43:03 +0200

    Add exponential decay LR scheduler

--- models/networks.py
+++ models/networks.py
@@ -52,6 +52,8 @@ def get_scheduler(optimizer, opt):
         scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, threshold=0.01, patience=5)
     elif opt.lr_policy == 'cosine':
         scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=opt.niter, eta_min=0)
+    elif opt.lr_policy == 'decay':
+        scheduler = lr_scheduler.ExponentialLR(optimizer, gamma=opt.gamma)
     else:
         return NotImplementedError('learning rate policy [%s] is not implemented', opt.lr_policy)
     return scheduler
